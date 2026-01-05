cb8cbf798bce323a04fef07e0b6d03b3a93c9b20
jestyug <eminenceuganda@gmail.com>
2026-01-05 06:58:46 +0000

    update 1

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.secret_key = 'dev-secret-key'
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/')
+def login():
+    return render_template('sys_login.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
